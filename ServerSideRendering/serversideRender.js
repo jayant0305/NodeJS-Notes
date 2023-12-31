5ac7b7a53223fe5d87cde6d8112d8bdb2ccfdc1b
jayant0305 <jayantbhatia03@gmail.com>
2023-12-31 12:18:03 +0530

    Server Side Rendering EJS

---
+++ ServerSideRendering/serversideRender.js
@@ -0,0 +1,17 @@
+//Use template engines for SSR
+// 1. EJS
+// 2. handlebars
+// 3. pug
+const express = require('express')
+const app = express()
+const path=require('path')
+
+app.set("view engine", "ejs")
+app.set("views",path.resolve("./views"))
+
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+app.listen(8080,()=>{
+    console.log("listening on 8080")
+})
